685ea91caee4d442b1f284826e4682c334729a1f
ミューズ <ivan.bolgar@gmail.com>
2013-07-04 22:48:34 +0300

    add more specs

--- spec/lib/feedly_api_spec.rb
+++ spec/lib/feedly_api_spec.rb
@@ -27,20 +27,29 @@ describe FeedlyApi::Feed do
   describe '#items' do
     let(:feed) { FeedlyApi::Feed.new 'https://www.eff.org/rss/updates.xml' }
 
-    it 'returns 20 feed items be defaul' do
-      expect(feed.items.length).to eq 20
+    context 'valid params' do
+      it 'returns 20 feed items by default' do
+        expect(feed.items.length).to eq 20
+      end
+
+      it 'takes :count param to get more or less feed items' do
+        expect(feed.items(count: 50).length).to eq 50
+      end
+
+      it 'takes :ranked param with value "oldest" and returns oldest items first' do
+        items = feed.items(ranked: 'oldest')
+        expect(items.first[:published] < items.last[:published]).to be_true
+      end
     end
 
-    it 'takes :count param to get more or less feed items' do
-      expect(feed.items(count: 50).length).to eq 50
-    end
+    context 'not valid params' do
+      it 'returns 0 items for negative :count param' do
+        expect(feed.items(count: -50).length).to eq 0
+      end
 
-    it 'returns 0 items for negative :count param' do
-      expect(feed.items(count: -50).length).to eq 0
-    end
-
-    it 'return defaul count of items (20) for non integer string' do
-      expect(feed.items(count: 'NOT_AN_INTEGER').length).to eq 20
+      it 'return defaul count of items (20) for non integer value' do
+        expect(feed.items(count: 'NOT_AN_INTEGER').length).to eq 20
+      end
     end
   end
 end
